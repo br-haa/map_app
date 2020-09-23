1aeb941786dfd4cca4c0a0905de34a4d806cc1d0
Benjamin Russell <benjamin.russell@hoglundlaw.hoglunddomain>
2020-09-23 15:57:41 -0500

    goodbye particles

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,6 @@
     <Nuxt />
     <client-only>
       <app-body></app-body>
-      <particles></particles>
     </client-only>
   </div>
 </template>
@@ -11,21 +10,10 @@
 
 <script>
 import AppBody from "~/components/AppBody";
-import Particles from "~/components/Particles.vue";
 export default {
-  components: {Particles, AppBody}
+  components: {AppBody}
 }
 </script>
 <style scoped lang="scss">
-div[id^="particles-instance-"] {
-  height: 100vh !important;
-  width: 100vw !important;
-  position: fixed !important;
-  top: 0 !important;
-  margin: 0 !important;
-  padding: 0 !important;
-  overflow: hidden !important;
-  background: rgba($color: white, $alpha: 0.4);
-  z-index: -1;
-}
+
 </style>
